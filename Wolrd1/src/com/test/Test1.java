f0f0fbc20c0dd8e32c3d6d63b525f9dd03da0479
moimaipso2 <motoyasu.imai.pso2@gmail.com>
2025-06-20 21:52:05 +0900

    練習用のプロジェクトをコミット

---
+++ Wolrd1/src/com/test/Test1.java
@@ -0,0 +1,18 @@
+/**
+ *  2025 Copyright.
+ */
+package com.test;
+
+/**
+ * 
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("★");
+	}
+
+}
